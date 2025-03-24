feb0d606f3ddf6edd4dc4b4ae215a17bef2aa39c
David Vrhovac <david.vrhovac1@gmail.com>
2025-03-24 21:58:29 +0100

    feat(videoPage): add video page #MDE-13 In Progress

---
+++ meditateeasy/src/main/java/med/easy/meditateeasy/view/VideoPresenter.java
@@ -0,0 +1,46 @@
+package med.easy.meditateeasy.view;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class VideoPresenter {
+    private final VideoView view;
+
+    private VideoPresenter(VideoView view) {
+        this.view = view;
+        attachEvents();
+    }
+
+    private void attachEvents() {
+        // open video page
+        view.getVideoBtn().setOnAction(event -> {
+            VideoPresenter.show(view.getStage());
+        });
+
+        // open instruction page
+        view.getInstructionBtn().setOnAction(event -> {
+            InstructionPresenter.show(view.getStage());
+        });
+
+        // open start page
+        view.getHomeBtn().setOnAction(event -> {
+            StartPresenter.show(view.getStage());
+        });
+    }
+
+    public static void show(Stage stage) {
+        VideoView view = new VideoView();
+        VideoPresenter controller = new VideoPresenter(view);
+
+        Scene scene = new Scene(view.getRoot());
+        scene.getStylesheets().add(Objects.requireNonNull(VideoPresenter.class.getResource("/videoPage.css")).toExternalForm());
+        stage.setTitle("Meditate Easy");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
