7ae9f33e6431cf1ceb66a7786e969fdf44b13ed4
David Vrhovac <david.vrhovac1@gmail.com>
2025-06-14 14:50:56 +0200

    feat: add stopwatch feature into instruction and video detail views #MDE-27 In Progress

--- meditateeasy/src/main/java/med/easy/meditateeasy/view/instruction/InstructionDetailView.java
+++ meditateeasy/src/main/java/med/easy/meditateeasy/view/instruction/InstructionDetailView.java
@@ -1,11 +1,14 @@
 package med.easy.meditateeasy.view.instruction;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import med.easy.meditateeasy.MeditateEasyApp;
 import med.easy.meditateeasy.model.Instruction;
+import med.easy.meditateeasy.util.Stopwatch;
 
 public class InstructionDetailView {
     private final VBox root = new VBox();
@@ -14,6 +17,9 @@ public class InstructionDetailView {
     private final Label titleLabel = new Label();
     private final Label difficultyLabel = new Label();
     private final Label descriptionLabel = new Label();
+    private final Stopwatch stopwatch = new Stopwatch();
+    private final HBox contentContainer = new HBox(20);
+    private final VBox instructionContainer = new VBox(20);
 
     public InstructionDetailView() {
         root.setPrefWidth(MeditateEasyApp.getX());
@@ -29,8 +35,14 @@ public class InstructionDetailView {
         difficultyLabel.getStyleClass().add("detail-difficulty");
         descriptionLabel.getStyleClass().add("detail-description");
 
-        root.getChildren().addAll(header, titleLabel, difficultyLabel, descriptionLabel);
+        instructionContainer.getChildren().addAll(titleLabel, difficultyLabel, descriptionLabel);
 
+        stopwatch.setAlignment(Pos.TOP_CENTER);
+        
+        contentContainer.getChildren().addAll(instructionContainer, stopwatch);
+        root.getChildren().addAll(header, contentContainer);
+
+        root.getStylesheets().add(getClass().getResource("/stopwatch.css").toExternalForm());
     }
 
     public void setInstruction(Instruction instruction) {
--- meditateeasy/src/main/java/med/easy/meditateeasy/view/video/VideoDetailView.java
+++ meditateeasy/src/main/java/med/easy/meditateeasy/view/video/VideoDetailView.java
@@ -1,5 +1,6 @@
 package med.easy.meditateeasy.view.video;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -7,6 +8,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import med.easy.meditateeasy.MeditateEasyApp;
 import med.easy.meditateeasy.model.Video;
+import med.easy.meditateeasy.util.Stopwatch;
 
 public class VideoDetailView {
     private final VBox root = new VBox();
@@ -15,6 +17,9 @@ public class VideoDetailView {
     private final Label titleLabel = new Label();
     private final Label difficultyLabel = new Label();
     private final WebView webView = new WebView();
+    private final Stopwatch stopwatch = new Stopwatch();
+    private final HBox contentContainer = new HBox(20);
+    private final VBox videoContainer = new VBox(20);
 
     public VideoDetailView() {
         header.getChildren().add(backButton);
@@ -26,7 +31,14 @@ public class VideoDetailView {
         titleLabel.getStyleClass().add("detail-title");
         difficultyLabel.getStyleClass().add("detail-difficulty");
 
-        root.getChildren().addAll(header, titleLabel, difficultyLabel, webView);
+        contentContainer.setAlignment(Pos.CENTER);
+        stopwatch.setAlignment(Pos.TOP_CENTER);
+        contentContainer.getChildren().addAll(videoContainer, stopwatch);
+        videoContainer.getChildren().addAll(titleLabel, difficultyLabel, webView);
+
+        root.getChildren().addAll(header, contentContainer);
+
+        root.getStylesheets().add(getClass().getResource("/stopwatch.css").toExternalForm());
     }
 
     public void setVideo(Video video) {
